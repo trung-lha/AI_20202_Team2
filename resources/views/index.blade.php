72a8c100c4d44b12f5518e0e2ddc7cb9f1f13dd2
nguyethm <nguyet332747@gmail.com>
2021-05-29 23:26:17 +0700

    Pose Estimate UI

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+    @include('layouts.header')
+    <div id="about" class="row" style="font-family:courier;  margin-top: 150px; ">
+        <div class="col-6" style="margin-top: 50px">
+            <img style="width: 100%; margin : 5% " src="./img/intro.png ">
+        </div>
+        <div class="col-6" style="margin-top: 150px; background-color:white; ">
+            <h3> Human Pose Estimate Website</h3>
+            <p><i>
+                    The website counts the number of movements of each exercise that you practice every day, provides a tool
+                    to help you track your training progress.
+                </i></p>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mt-3">Join with Us</a>
+        </div>
+    </div>
+    <div class="row bg-light">
+        <img class="center" style="width: 80%;" src="./img/intro2.png ">
+    </div>
+    <div class="row">
+        <div class="col-4 content">
+            <b>Various exercises</b>
+        </div>
+        <div class="col-4 content" style="border-left: 0.5px solid #5a5c69; border-right: 0.5px solid #5a5c69">
+            <b>Using machine learning to support realtime counting</b>
+        </div>
+        <div class="col-4 content">
+            <b>Keep track of your exercise regimen</b>
+        </div>
+    </div>
+    <div style="font-family:courier; text-align: center; margin-bottom: 50px;">
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mt-3">Register Now</a>
+    </div>
+    @include('layouts.footer')
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<header class="navbar fixed-top bg-light" >
+    <div style=" width: 190%;">
+        <div>
+            <p class="navbar-text-top">
+                <br><b>Welcome to Human Pose Estimate Website!</b><br>
+                <hr>
+            </p>
+        </div>
+        <h3 class="mr-5">
+            <nav style="text-align: right">
+                <a href="#about" ><u>About Us</u></a>
+                <a href="#" ><u>Introduction</u></a>
+                <a href="register.html" ><u>Register</u></a>
+                <a href="login.html"><u>Login</u></a>
+                </ul>
+            </nav>
+        </h3>
+    </div>
+</header>
